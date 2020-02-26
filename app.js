f6f489ac0fa18c8aa9622f99c130a714ece4fc33
Sushil <bajracharya.sushil01@gmail.com>
2020-02-26 16:53:07 +0545

    create api created

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
+require("dotenv").config();
+
 const express = require('express');
 const app = express();
+const userRouter = require("./api/users/user.router");
 
 
-app.get("/api", (req, res) => {
-    res.json({
-        success: 1,
-        message: "This API is working."
-    });
-});
+app.use("/api/users", userRouter);
 
 
-app.listen(3000, ()=> {
-    console.log("Server is running at port 3000");
+
+
+app.listen(process.env.APP_PORT, ()=> {
+    console.log("Server is running at port: "+ process.env.APP_PORT);
 })
