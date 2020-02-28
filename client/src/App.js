69de00df815af0247ce744d1dc7610c40e92cff1
Sushil <bajracharya.sushil01@gmail.com>
2020-02-28 19:58:53 +0545

    React + Formik

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
+const App = ({
+  values,
+  handleChange
+}) => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="email" name="email" id="email" placeholder="Email" value={values.email} onChange={handleChange} />
+      <input type="password" name="password" id="password" placeholder="******" value={values.password} onChange={handleChange} />
     </div>
   );
 }
--- client/src/index.js
+++ client/src/index.js
@@ -2,9 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { withFormik } from 'formik';
+import Yup from 'yup';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import * as serviceWorker from './serviceWorker';
+const FormikApp = withFormik({
+    mapPropsToValues() {
+        return{
+            email: 'abc@xyz.com',
+            password: '*******'
+        }
+    }
+})(App);
+ReactDOM.render(<FormikApp />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
